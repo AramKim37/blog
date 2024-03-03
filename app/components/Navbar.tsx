b5049c023d899a68d401e0e0613aade25ca80755
AramKim37 <aramkim37@gmail.com>
2024-03-03 13:51:45 -0700

    completed darkmode

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="w-full flex justify-between items-center px-4 py-5">
       <h1>
         This is <span className="text-primary font-bold">navbar</span>
       </h1>
+      <ModeToggle />
     </nav>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-between max-w-2xl px-5 bg-blue-100 mx-auto">
-      <Navbar />
+    <div className="w-full flex flex-col  max-w-2xl px-5 mx-auto">
       <h1>hello this is blog main page</h1>
     </div>
   );
