5bc620d9055453281cb7b89f1901c7ca7ce68ba7
AramKim37 <aramkim37@gmail.com>
2024-03-02 21:11:14 -0700

    set up layout all items in the center line

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full flex flex-col items-center justify-between max-w-2xl px-5 bg-blue-100 mx-auto">
       <Navbar />
       <h1>hello this is blog main page</h1>
     </div>
