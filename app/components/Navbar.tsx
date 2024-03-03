5e56b246d13a350e49414a3df5dfe65c30e6f615
AramKim37 <aramkim37@gmail.com>
2024-03-03 12:34:00 -0700

    installed shadcn package for colors, darkmode and some components buttons..

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav>
-      <h1>This is navbar</h1>
+      <h1>
+        This is <span className="text-primary font-bold">navbar</span>
+      </h1>
     </nav>
   );
 };
