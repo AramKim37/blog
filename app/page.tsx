dc4859dd85cb996107c08f233e05ea490a4e8c6b
AramKim37 <aramkim37@gmail.com>
2024-03-04 21:26:55 -0700

    fixed images

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,20 @@ async function getData() {
 
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
-  console.log(data);
   return (
-    <div>
+    <div className="grid grid-cols-2  mx-auto">
       {data.map((post, idx) => (
-        <Image
-          key={idx}
-          src={urlFor(post.titleImage).url()}
-          alt="image"
-          width={100}
-          height={100}
-        />
+        <div key={idx} className="flex flex-col items-center justify-center">
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="image"
+            width={300}
+            height={300}
+            className="object-cover object-center w-full h-full "
+          />
+          <h1>{post.title}</h1>
+          <p>{post.smallDescription}</p>
+        </div>
       ))}
     </div>
   );
